f9623ce62aba9e1b4f7cff0132c984bb5ff592b5
Zheng UET <zhenguet@gmail.com>
2020-11-12 14:15:22 +0700

    update

--- app/containers/RequestForm/ImageUpload.js
+++ app/containers/RequestForm/ImageUpload.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-function ImageUpload(props) {
+function ImageUpload() {
+  const [selectedFile, setSelectedFile] = useState();
+  const [preview, setPreview] = useState();
+
+  // Upload Image
+  useEffect(() => {
+    if (!selectedFile) {
+      setPreview(undefined);
+      return;
+    }
+
+    const objectUrl = URL.createObjectURL(selectedFile);
+    setPreview(objectUrl);
+  }, [selectedFile]);
+
+  // Select Image
+  const onSelectFile = e => {
+    if (!e.target.files || e.target.files.length === 0) {
+      setSelectedFile(undefined);
+      return;
+    }
+
+    setSelectedFile(e.target.files[0]);
+  };
+
   return (
     <div>
-      
+      <input
+        id="input-upload-image"
+        className="imageUpload"
+        type="file"
+        onChange={onSelectFile}
+      />
+      {selectedFile && <img alt="" className="img" src={preview} />}
     </div>
   );
 }
